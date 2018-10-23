b9f384d1d52100c45acad657030fb133ac35fc28
Sewon Woo <sewon.dev@gmail.com>
2018-10-23 17:49:54 +0900

    Add routing module

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { MainComponent } from './main/main.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: 'main', component: MainComponent },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+  	RouterModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
