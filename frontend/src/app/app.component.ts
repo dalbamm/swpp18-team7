9c7544f01e58f223b6ed449e235b037c0e20159c
Sewon Woo <sewon.dev@gmail.com>
2018-10-23 17:51:40 +0900

    Update app.component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+import { User } from './user';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Boogle';
+  user: User = null;
+  isMain: boolean = true;
+
+  goSignIn(): void {
+  	alert("sign-in button is clicked");
+  }
+
+  goUserInfo(): void {
+  	alert("user-info button is clicked");
+  }
 }
