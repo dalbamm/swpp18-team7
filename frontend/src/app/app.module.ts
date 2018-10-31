3313e2c08cc804a90b23a538ed10ca6310e65bb1
dalbamm <oscar.yoon13@gmail.com>
2018-10-31 17:58:15 +0900

    Initialize buttons and methods, and Import FormsModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
@@ -13,6 +14,7 @@ import { SearchComponent } from './search/search.component';
     SearchComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -7,15 +7,25 @@ import { Router } from '@angular/router';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  
+  searchQueryStr: string //Will be binded with searchInput
   constructor(
     private router: Router
   ) { }
 
   ngOnInit() {
     // Check if the user is authenticated or not
-    //if()
-  	//document.write('Hello')
+    //if(authenticated == false)  router.navigateByUrl('')
   }
-  
+  goSalePage(){
+    alert("goSalePage clicked")
+  }
+  onClickSearch(){
+    alert("You want to search "+this.searchQueryStr)
+  }
+  onClickGoDirect(){
+    alert("GoDirect clicked")
+  }
+  onClickInterested(){
+    alert("Interested clicked")
+  }
 }
