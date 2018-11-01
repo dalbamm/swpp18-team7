f85d82e9900a943e095019c92cfb0df12e5f1017
Young Min KIM <bahducoup@gmail.com>
2018-11-01 15:01:47 +0900

    add signin method in views

--- backend/boogle/urls.py
+++ backend/boogle/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from boogle import views
 
 urlpatterns = [
-    #path('signin', views.signin, name='signin')
+    path('signin', views.signin, name='signin')
 ]
--- backend/boogle/views.py
+++ backend/boogle/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotAllowed
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.core.exceptions import ObjectDoesNotExist
+import json
 
-# Create your views here.
+def signin(request):
+    if request.method == 'POST':
+        req_data = json.loads(request.body.decode())
+        username = req_data['username']
+        password = req_data['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponse(status=204)
+        else:
+            return httpResponse(status=401)
+
+    else:
+        return HttpResponseNotAllowed(['POST'])
+    
