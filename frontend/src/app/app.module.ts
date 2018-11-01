0da5e1ee8a58e5352e952e6232c9cf5b56fc2eb4
Young Min KIM <bahducoup@gmail.com>
2018-11-01 12:54:03 +0900

    temporary

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
