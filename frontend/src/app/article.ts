1314991bfb582804c7019838ccd9d113a43703c9
dalbamm <oscar.yoon13@gmail.com>
2018-11-01 17:02:56 +0900

    Update Article,User class and Initialize Comment class

--- frontend/src/app/article.ts
+++ frontend/src/app/article.ts
@@ -1,14 +1,16 @@
 import { User } from './user'
 import { Book } from './book'
+import { Comment } from './comment'
 
 export class Article {
     id: number;
     title: string;
     content: string;
-    author: User;
+    articleAuthor: User;
     
     book: Book;
     bookStatus: string;
     price: number;
+    commentList?: Comment[];
   }
   
