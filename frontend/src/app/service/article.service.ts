f6632d10a266a770f48c5543ccaf5774c93aa058
dalbamm <oscar.yoon13@gmail.com>
2018-11-11 11:47:01 +0900

    Initialize ArticleService

---
+++ frontend/src/app/service/article.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Response } from '@angular/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { User } from '../models/user';
+import { Article } from '../models/article';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticleService {
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization': 'my-auth-token',
+    })
+  };
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+    ) {}
+
+}
