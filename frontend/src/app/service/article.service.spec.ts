74cad638d9e6be520fab409dc999b2fae0189468
dalbamm <oscar.yoon13@gmail.com>
2018-11-13 13:29:23 +0900

    Implemet ArticleService Test codes

--- frontend/src/app/service/article.service.spec.ts
+++ frontend/src/app/service/article.service.spec.ts
@@ -1,15 +1,40 @@
-import { TestBed, inject } from '@angular/core/testing';
-
+import { async, TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Router } from '@angular/router';
 import { ArticleService } from './article.service';
+import { Article } from '../models/article';
+
+let articleService: ArticleService;
+let httpTestingController: HttpTestingController;
+let routerSpy;
 
 describe('ArticleService', () => {
   beforeEach(() => {
+    routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+
     TestBed.configureTestingModule({
-      providers: [ArticleService]
+      imports: [ HttpClientTestingModule ],
+      providers: [
+        ArticleService,
+        {provide: Router, useValue: routerSpy}]
     });
+
+    articleService = TestBed.get(ArticleService);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
   it('should be created', inject([ArticleService], (service: ArticleService) => {
     expect(service).toBeTruthy();
   }));
 });
+
+describe(': getExternalArticles', () => {
+  it('should sign in user', async(() => {
+    const testIsbn = '123456789';
+    articleService.getExternalArticles(testIsbn);
+    const reqGetExternalArticle = httpTestingController.expectOne(articleService.url + testIsbn);
+    expect(reqGetExternalArticle.request.method).toEqual('GET');
+    // reqGetExternalArticle.flush({status: 204});
+  }));
+});
+
